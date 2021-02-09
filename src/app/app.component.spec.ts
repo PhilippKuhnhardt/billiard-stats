18b27bee926fa6a53ebe7dfa3eacc5365f9703a0
PhilippKuhnhardt <56841270+PhilippKuhnhardt@users.noreply.github.com>
2021-02-09 20:21:03 +0100

    Added basic test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,32 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Title } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+  let titleService: Title;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [
+        Title
+      ]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'billiardStats'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('billiardStats');
+  it('should be able to set title', () => {
+    titleService = TestBed.inject(Title);
+    const titleText = 'Test-Titel';
+    app.setTitle(titleText);
+    expect(titleService.getTitle()).toEqual(titleText);
   });
 });
